fd9bcc71d021f4f06e62547208beac112ee2d0b7
Jikku Jose <jikku@storybrain.com>
2024-05-03 22:10:47 +0530

    Fix layout

--- vote/components/Avatar/index.js
+++ vote/components/Avatar/index.js
@@ -1,12 +1,13 @@
 import { Image } from "./Image.js"
 import { TextBlock } from "./TextBlock.js"
+import { Ballot } from "./Ballot.js"
 
-export function Avatar({ title, subtitle, url, ballot }) {
+export function Avatar({ title, subtitle, url, initialVote }) {
   return {
-    ballot: ballot,
+    ballot: Ballot({ n: initialVote }),
     $template: /* HTML */ `
       <div
-        class="p-4 bg-gray-100 t bg-opacity-10 text-white shadow rounded-lg my-4 border border-yellow-300 border-opacity-10 hover:border-opacity-60"
+        class="p-4 bg-gray-100 t bg-opacity-10 text-white shadow rounded-lg mb-2 border border-yellow-300 border-opacity-10 hover:border-opacity-60"
         style="min-width: 180px;"
       >
         ${TextBlock({ title, subtitle }).$template} ${Image({ url }).$template}
