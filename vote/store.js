b021c5882a1f0622a65498354b221ab335da9258
Jikku Jose <jikku@storybrain.com>
2024-05-03 22:33:42 +0530

    Refactor

---
+++ vote/store.js
@@ -0,0 +1,24 @@
+window.store = {
+  avatars: [
+    {
+      title: "Shilpa John",
+      subtitle: "Web Developer",
+      url: "https://images.unsplash.com/photo-1591760852592-4a0665d280b4?q=80&fm=jpg&crop=faces&fit=crop&h=300&w=300",
+    },
+    {
+      title: "Susan Smith",
+      subtitle: "UX Designer",
+      url: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=100&fm=jpg&crop=faces&fit=crop&h=300&w=300",
+    },
+    {
+      title: "Annie Johnson",
+      subtitle: "Project Manager",
+      url: "https://images.unsplash.com/photo-1602233158242-3ba0ac4d2167?q=80&fm=jpg&crop=faces&fit=crop&h=300&w=300",
+    },
+    {
+      title: "Nelly Walson",
+      subtitle: "Photographer",
+      url: "https://images.unsplash.com/photo-1578979879663-4ba6a968a50a?q=100&fm=jpg&crop=faces&fit=crop&h=300&w=300",
+    },
+  ],
+}
