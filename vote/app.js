1bafec701e62b8234de7a68ac900bbe2a43c834a
Jikku Jose <jikku@storybrain.com>
2024-05-04 10:30:44 +0530

    Revert to local component

--- vote/app.js
+++ vote/app.js
@@ -1,8 +1,6 @@
 import { createApp } from "https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js"
 import { Avatar } from "./components/Avatar/index.js"
-import { SFAvatar } from "https://cdn.toolbomber.com/pv-components/SFAvatar.js"
 
 createApp({
   Avatar,
-  SFAvatar,
 }).mount()
